f31891cb81d5f3372886567b6bf7b5e905d8ca25
rjomandam566 <55949915+rjomandam566@users.noreply.github.com>
2021-02-21 16:16:50 +0800

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+int main1() {
+	// Number 1
+	/*This program calculates and divides the first and second numbers given
+	int x = 33;
+	int y = 7;
+	float z = (float)x / y;
+	//I put a parenthesis (float) before the x so that it will be converted into a data type float.
+	printf("Answer is %f\n", z);
+	
+	*/
+
+
+
+	int english, filipino, science, math, mapeh;
+
+	english = 95;
+	filipino = 89;
+	science = 92;
+	math = 90;
+	mapeh = 88;
+
+	int addSub = english + filipino + science + math + mapeh;
+
+	float Ave = (float)addSub / 5;
+
+	printf("The average is: %f\n", Ave);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//Number 2
+	/*Conversion between type int and type char
+	
+	// I declared a type int and converted it using the parenthesis (char) and assigning 56 as the number.
+	int a = (char)56;
+
+	// I declared a type char and converted it using the parenthesis (int) and assigning 'a' as the char.
+	char x = (int)'a';
+
+	printf("int to char: %c\nchar to int %d", a, x);
+	
+	*/
+
+
+	//Number 3
+	/*Demonstration of non printable control Characters (ASCII)*/
+	//I first declared two data type char
+	/*
+	char a, b;
+	a = (char)9;
+	b = (char)8;
+
+	printf("A horizontal%cTab\nA lette%cr will be lost\n", a, b);
+	*/
+
+	/*when data type char are converted into int, it was converted into non printable control characters which
+	* has their own specific actions.
+	*/
+
+
+
+
+
+
+
+
+	//Number 4
+
+	
+}
